d3b8239ba0b88fc014ce532056d61e2d5fcb0727
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-03 22:34:06 -0400

    take out console logs

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@ $( document ).ready(function() {
     r = Math.floor((Math.random() * 254) + 1);
     g = Math.floor((Math.random() * 254) + 1);
     b = Math.floor((Math.random() * 254) + 1);
-    console.log(r);
-    console.log(g);
-    console.log(b);
-    console.log("rgb(" + r + "," + g + "," + b + ")");
     $(this).css("background-color","rgb(" + r + "," + g + "," + b + ")");
     ele = $('div.color_info h3').length;
     if ( ele == 0 ) {
